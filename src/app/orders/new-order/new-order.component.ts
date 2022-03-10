12578a6cf4e444bc3f6ef1295988cf6903bcf2f0
Michał Pruchniak <goldenofheart@gmail.com>
2022-03-10 19:36:44 +0000

    validate new order

--- src/app/orders/new-order/new-order.component.ts
+++ src/app/orders/new-order/new-order.component.ts
@@ -20,8 +20,13 @@ export class NewOrderComponent {
   addOrderForm = this.fb.group({
     client_id: ['', [Validators.required]],
     courier: ['', [Validators.required]],
-    address: ['', [Validators.required]],
-    phone: ['', [Validators.required]],
+    address: ['', [Validators.required,
+                   Validators.minLength(6)
+                  ]],
+    phone: ['', [Validators.required,
+                 Validators.pattern('[+0-9]{9,12}')
+                ]
+           ],
     salesChannel: ['', [Validators.required]],
   });
 
